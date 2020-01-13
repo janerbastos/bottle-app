74a1289dd337af4378ba3b454c972946f34cf717
Janer B. Melo <janerbastos@gmail.com>
2020-01-13 13:17:15 -0300

    Configuração do novo metodo de nos forms

--- core/forms/validate/form.py
+++ core/forms/validate/form.py
@@ -1,25 +1,34 @@
 class Form(object):
 
+    fields = []
+
     def __init__(self, form, Class, instance=None):
         self.form = form
         self.instance = instance
         self.Class = Class
+        self.errors = None
+        self.data = None
 
-    def load_data(self, fields):
-        data = {}
-        for field in fields:
-            data[field] = self.form.get(field)
-        return data
+    def load_data(self):
+        self.data = {}
+        for field in self.fields:
+            self.data[field] = self.form.get(field)
 
-    def save(self, fields):
-        data = self.load_data(fields)
-        print(data)
-        model = self.Class(**data)
+    def save(self):
+        self.load_data()
+        model = self.Class(**self.data)
         return model
 
     def is_valid(self):
-        pass
-
+        self.errors = {}
+        for field in self.fields:
+            value = self.form.get(field)
+            if value is "" or value is None:
+                self.errors[field] = "Você deve inserir um valor para <strong>%s</strong> no formulário" % field
+            self.data[field] = value
+        if self.errors:
+            return False
+        return True
 
 def required_field(form, required):
     result = {'error': None, 'data': None}
--- login/controllers/security.py
+++ login/controllers/security.py
@@ -1,7 +1,8 @@
 
 from core import app, views
+from login.forns import LoginForm
 from core.decorators import login_required
-from core.forms.validate.form import required_field, Form
+from core.forms.validate.form import required_field
 from bottle import redirect, request, response
 from login.models.auth import User
 from bottle_utils.csrf import csrf_protect, csrf_token, csrf_tag
@@ -36,6 +37,7 @@ def do_login(db, session):
             return redirect('/blog')
     return {'message': 'Usuário ou senha podem esta errados.', 'code': 'warning'}
 
+
 @app.route('/logout')
 @login_required
 def logout(session):
@@ -61,30 +63,28 @@ def register():
 @csrf_token
 def do_register(db):
     
-    username = request.forms.get('username')
     confirma_password = request.forms.get('confirma_password')
-
-    result = required_field(request.forms,
-            ['first_name', 'last_name', 'username', 'email', 'password'])
+    form = LoginForm(request.forms, User)
     
-    if result.get('error'):
-        return {'error': result.get('error'), 'data': result.get('data'), 'code': 'danger', 'token': request.csrf_token}
+    # result = required_field(request.forms,
+    #         ['first_name', 'last_name', 'username', 'email', 'password'])
+    # if result.get('error'):
+    #     return {'error': result.get('error'), 'data': result.get('data'), 'code': 'danger', 'token': request.csrf_token}
 
+    if form.is_valid():
+        usuario_exist = db.query(User).filter_by(username=form.data.get('username').first()
+        if usuario_exist:
+            return {'error': result.get('error'), 'data': result.get('data'), 'message': 'Usuário exite.', 'code': 'danger', 'token': request.csrf_token}
 
-    usuario_exist = db.query(User).filter_by(username=username).first()
-    if usuario_exist:
-        return {'error': result.get('error'), 'data': result.get('data'), 'message': 'Usuário exite.', 'code': 'danger', 'token': request.csrf_token}
+        user = form.save()
+        if not user.check_password(confirma_password):
+            return {'error': result.get('error'), 'data': result.get('data'),'message': 'Senha não confirma.', 'code': 'danger', 'token': request.csrf_token}
 
-    form = Form(request.forms, User)
-    user = form.save(['first_name', 'last_name', 'username', 'email', 'password'])
-    if not user.check_password(confirma_password):
-        return {'error': result.get('error'), 'data': result.get('data'),'message': 'Senha não confirma.', 'code': 'danger', 'token': request.csrf_token}
-
-    usuario_email_exist = db.query(User).filter_by(email=user.email).first()
-    if usuario_email_exist:
-        return {'error': result.get('error'), 'data': result.get('data'),
-            'message': 'Email de usuário já esta sendo usado por outro usuário.', 'code': 'danger', 'token': request.csrf_token}
-    
-    db.add(user)
+        usuario_email_exist = db.query(User).filter_by(email=user.email).first()
+        if usuario_email_exist:
+            return {'error': result.get('error'), 'data': result.get('data'),
+                'message': 'Email de usuário já esta sendo usado por outro usuário.', 'code': 'danger', 'token': request.csrf_token}
+        
+        db.add(user)
 
     return {'data': None, 'error': None, 'message': 'Usuário registrado com sucesso.', 'code': 'success', 'token': request.csrf_token}
